12eb931c0be066e6a6b9e1508a2f9c870d2c53f9
Christian Lang <lang.chr86@gmail.com>
2023-07-20 15:37:28 +0200

    * make docker image build script independent from working directory

--- docker-image/build.sh
+++ docker-image/build.sh
@@ -1,3 +1,5 @@
 #! /bin/bash -e
 
-podman build -t ghcr.io/langchr86/docmake:latest -f ./Dockerfile ..
+SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+podman build -t ghcr.io/langchr86/docmake:latest --file ${SCRIPT_DIR}/Dockerfile ${SCRIPT_DIR}/..
